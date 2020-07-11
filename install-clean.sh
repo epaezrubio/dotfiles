e054cb62957ce4939bdd4a7af32b62a50c28d89b
Eduardo Paez Rubio <contact@devpaezrubio.com>
2020-07-11 16:28:59 +0000

    Add installation script

---
+++ install-clean.sh
@@ -0,0 +1,6 @@
+rm $HOME/.gitconfig
+rm $HOME/.bashrc
+rm $HOME/.vimrc
+rm $HOME/.tmux.conf
+
+./install.sh
---
+++ install.sh
@@ -0,0 +1,4 @@
+cat gitconfig >> $HOME/.gitconfig
+cat profile >> $HOME/.bashrc
+cat vimrc >> $HOME/.vimrc
+cat tmux.conf >> $HOME/.tmux.conf
